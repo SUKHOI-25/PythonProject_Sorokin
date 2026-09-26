3eaa08887ba4e637179f46ac1f692b3de476697e
SUKHOI-25 <dust66609@gmail.com>
2026-09-26 13:39:45 +0300

    pep8 chages

--- pract1ig.py
+++ pract1ig.py
@@ -1,14 +1,14 @@
 try:
     while True:
         try:
-            n=int(input("Введите количество секунд прошедших с начала суток: "))
+            n = int(input("Введите количество секунд прошедших с начала суток: "))
             if n>0:
-                n%=86400 # удаляем день если секунд больше чем в 24 часах
+                n %= 86400 # удаляем день если секунд больше чем в 24 часах
                 hours, remains = divmod(n, 3600) #hours-целая часть от деления (//), remains-остаток от деления в секундах (%)
                 minutes, seconds = divmod(remains, 60)
                 print(f"h:{hours:02d}, m:{minutes:02d}, s:{seconds:02d}")
                 break
-            elif n==0:
+            elif n == 0:
                 print("Ни часов ни минут ни секунд не прошло")
                 continue
             elif n<0:
