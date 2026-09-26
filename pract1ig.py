985176ca7034addc0e1d49e1aa936d3d1067f528
SUKHOI-25 <dust66609@gmail.com>
2026-09-26 13:40:30 +0300

    also pep8

--- pract1ig.py
+++ pract1ig.py
@@ -2,7 +2,7 @@ try:
     while True:
         try:
             n = int(input("Введите количество секунд прошедших с начала суток: "))
-            if n>0:
+            if n > 0:
                 n %= 86400 # удаляем день если секунд больше чем в 24 часах
                 hours, remains = divmod(n, 3600) #hours-целая часть от деления (//), remains-остаток от деления в секундах (%)
                 minutes, seconds = divmod(remains, 60)
@@ -11,7 +11,7 @@ try:
             elif n == 0:
                 print("Ни часов ни минут ни секунд не прошло")
                 continue
-            elif n<0:
+            elif n < 0:
                 print("Некорректное время")
                 continue
         except ValueError:
