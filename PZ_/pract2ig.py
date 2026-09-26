4792b42f279f62628835c4cb30105b7759504161
SUKHOI-25 <dust66609@gmail.com>
2026-09-26 13:49:58 +0300

    перераспределение

---
+++ PZ_/pract2ig.py
@@ -0,0 +1,23 @@
+#Вариант 25. С начала суток прошло N секунд (N — целое). Найти количество полных часов, прошедших с начала суток.
+try:
+    while True:
+        try:
+            n = int(input("Введите количество секунд прошедших с начала суток: "))
+            if n > 0:
+                n %= 86400 # удаляем день если секунд больше чем в 24 часах
+                hours, remains = divmod(n, 3600) #hours-целая часть от деления (//), remains-остаток от деления в секундах (%)
+                minutes, seconds = divmod(remains, 60)
+                print(f"h:{hours:02d}, m:{minutes:02d}, s:{seconds:02d}")
+                break
+            elif n == 0:
+                print("Ни часов ни минут ни секунд не прошло")
+                continue
+            elif n < 0:
+                print("Некорректное время")
+                continue
+        except ValueError:
+            print("Это не целое число")
+        except EOFError: #ctrl+d пустой ввод (на винде ctrl+z)
+            print("Что-то натыкали в консоли")
+except KeyboardInterrupt: #ctrl+c жесткое прерывание (перерывание через ctrl+z не обрабатывается)
+    print("\n\nПрограмма прервана пользователем")
